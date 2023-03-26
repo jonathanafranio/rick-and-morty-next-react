4f202f3ae8712e2d2b20f13796075b702afb9b68
jonathanafranio <jonathan.afranio.s@gmail.com>
2023-03-26 04:07:04 -0300

    feat: head, title e favicon

--- src/components/Layouts/LayoutDefault.jsx
+++ src/components/Layouts/LayoutDefault.jsx
@@ -1,18 +1,25 @@
 import React from "react"
-
+import Head from 'next/head'
 import Header from './Header';
 import Footer from './Footer';
 const LayoutDefault = (props) => {
     return(
-        <div>
-            <Header />
-            <main className="container">
-                <section className="page-section">
-                    { props.children }
-                </section>
-            </main>
-            <Footer />
-        </div>
+        <>
+            <Head>
+                <title>Rick And Morty</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div>
+                <Header />
+                <main className="container">
+                    <section className="page-section">
+                        { props.children }
+                    </section>
+                </main>
+                <Footer />
+            </div>
+        </>
     )
 }
 export default LayoutDefault;
